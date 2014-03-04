534f79b6af560788800973891aa136235d9b003c
Chonel LaPorte <chonellaporte@OSXLAP01612.local>
2014-03-03 21:24:30 -0500

    working joystick

--- src/components/joystick_test.js
+++ src/components/joystick_test.js
@@ -1,20 +1,28 @@
 Crafty.c('JSBase', {
 	_stick:null,
 	_poly:null,
+    _center: {},
 	init: function () {
 		this.requires('2D, DOM, Color');
 		this.x = 0;
-		this.y = Crafty.viewport.h - this.h;
+		this.y = 0;
 		this.h = 198;
 		this.w = 198;
 		this.color('red');
 		if (!this._stick) {
-			this._stick = Crafty.e('Joystick');
-			this._stick.x = (this.w/3);
-			this._stick.y = (this.h/3);
-			this._stick._home = {x:this._stick.x, y:this._stick.y};
-			this._stick._parent = this;
+			var stick = this._stick = Crafty.e('Joystick')
+			stick.x = (this.w/3);
+			stick.y = (this.h/3);
+            stick.h = (this.h/3);
+            stick.w = (this.w/3);
+			stick._home = {x:stick.x, y:stick.y};
+			stick._parent = this;
+			stick._maxDistance = (this.w/2);
+            stick._xLimit = this.x + this.w
+            stick._yLimit = this.y + this.h
 		}
+		this._center.x = this.x + (this._w/2);
+		this._center.y = this.y + (this._h/2);
 	}
 });
 
@@ -25,13 +33,12 @@ Crafty.c('Joystick', {
 		this.color('black');
 		this.h = 50;
 		this.w = 50;
-		this._center.x = (this.w/2);
-		this._center.y = (this.h/2);
-		this._corners = [this.x, this.y, this.x + this.w, this.y + this.h];
 		this.bind('Dragging', function(e) {
-			//console.log(this._parent,this)
-			if (!this._parent.contains(this)) {
-				this.checkSide(e);
+            var centerX = this._center.x = this.x + (this.w/2);
+            var centerY = this._center.y = this.y + (this.h/2);
+			var parent = this._parent;
+            if (Crafty.math.distance(centerX, centerY, parent._center.x, parent._center.x) >= this._maxDistance) {
+				this.adjustPosition();
 			}
 		});
 		this.bind('StopDrag', function() {
@@ -40,10 +47,17 @@ Crafty.c('Joystick', {
 		});
 	},
 
-	checkSide: function(e) {
-		var corner = this._corners;
-		if (e.clientX > (this._parent.x + this._parent.w)) {
-			this.x = (this._parent.w - this.w)
-		}
+	adjustPosition: function() {
+        if (this._center.x >= this._xLimit) {
+            this.x = this._xLimit - (this.w/2);
+        } else if (this._center.x <= this._parent.x) {
+            this.x = this._parent.x - (this.w/2);
+        }
+
+        if (this._center.y >= this._yLimit) {
+            this.y = this._yLimit - (this.h/2);
+        } else if (this._center.y <= this._parent.y) {
+            this.y = this._parent.y - (this.h/2);
+        }
 	}
 });
